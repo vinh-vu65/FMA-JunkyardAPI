bc9433e42d0478b748f6b853c62344ef6aae2b54
Vinh Vu <vinh.vu65@gmail.com>
2022-09-20 10:58:49 +1000

    Add mapper for DTOs in Car domain model

---
+++ JunkyardWebApp.API/Mappers/CarMapper.cs
@@ -0,0 +1,36 @@
+using JunkyardWebApp.API.Dtos;
+using JunkyardWebApp.API.Models;
+
+namespace JunkyardWebApp.API.Mappers;
+
+public static class CarMapper
+{
+    public static Car ToEntity(this PostPutCarDto dto)
+    {
+        return new Car
+        {
+            Year = dto.Year,
+            Make = dto.Make,
+            Model = dto.Model
+        };
+    }
+
+    public static void UpdateWith(this Car car, PostPutCarDto dto)
+    {
+        car.Year = dto.Year;
+        car.Make = dto.Make;
+        car.Model = dto.Model;
+    }
+
+    public static GetCarDto ToDto(this Car car)
+    {
+        return new GetCarDto
+        {
+            CarId = car.CarId,
+            Year = car.Year,
+            Make = car.Make,
+            Model = car.Model,
+            AvailablePartsCount = car.AvailableParts.Count 
+        };
+    }
+}
