65a235d9e5e92968b2338991cb6a25226bc4b9c0
Vinh Vu <vinh.vu65@gmail.com>
2022-09-28 11:15:37 +1000

    Add carID to part Dtos

--- JunkyardWebApp.API/Dtos/GetPartDto.cs
+++ JunkyardWebApp.API/Dtos/GetPartDto.cs
@@ -4,6 +4,8 @@ namespace JunkyardWebApp.API.Dtos;
 
 public class GetPartDto
 {
+    public int CarId { get; set; }
+    public int PartId { get; set; }
     public int Year { get; set; }
     public string Make { get; set; }
     public string Model { get; set; }
