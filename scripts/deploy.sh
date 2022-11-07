33736b2382f78756054f137cd877c7665e99d4e1
Vinh Vu <vinh.vu65@gmail.com>
2022-11-07 14:50:27 +1100

    try new yml path in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ktmpl ../deploy/template.yml -f ../deploy/default.yml --parameter imageTag ${BUILDKITE_COMMIT} | kubectl apply -f -
+ktmpl deploy/template.yml -f deploy/default.yml --parameter imageTag "${BUILDKITE_COMMIT}" | kubectl apply -f -
