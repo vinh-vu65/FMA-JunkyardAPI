200b531c2ed16810037699cefa7f1f87a4121198
Vinh Vu <vinh.vu65@gmail.com>
2022-11-09 09:29:37 +1100

    add database and secrets to deployment yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ktmpl deploy/template.yml -f deploy/arguments.yml --parameter imageTag "${BUILDKITE_COMMIT}" | kubectl apply -f -
+ktmpl deploy/jupiter.yml -f deploy/arguments.yml --parameter imageTag "${BUILDKITE_COMMIT}" | kubectl apply -f -
